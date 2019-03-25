90d1b2c0b6e1354f96f6cbe6f00aa2f2568664eb
Alper Yilmaz <alperyilmaz@gmail.com>
2019-03-26 02:27:39 +0300

    initial upload

---
+++ binder/install.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+install.packages("tidygraph")
+install.packages("igraph")
+install.packages("ggraph")
+install.packages("BiocManager")
+BiocManager::install("ChemmineOB", version = "3.8")
+BiocManager::install("ChemmineR", version = "3.8")
+BiocManager::install("fmcsR") 
+
